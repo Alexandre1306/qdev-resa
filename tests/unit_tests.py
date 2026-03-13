bb76314413b496daab0ab272975815f936408ff9
Alexandre Meira <22403726@me.uvsq.fr>
2026-03-13 15:57:17 +0100

    ajout test pour les salles petites/moyennes/grandes

--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -4,6 +4,10 @@ from resa.resa import *
 class bookMeetingRoomUnitTests(unittest.TestCase):
   def test_small_room(self):
     self.assertEqual(bookMeetingRoom(5), Room.SMALL)
+def test_medium_room(self):
+    self.assertEqual(bookMeetingRoom(10), Room.MEDIUM)
+def test_large_room(self):
+    self.assertEqual(bookMeetingRoom(100), Room.LARGE)
 
 if __name__ == '__main__':
   unittest.main()
